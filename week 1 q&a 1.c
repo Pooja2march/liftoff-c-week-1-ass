e6dded6195f584f748f89371d8fd23b2480b6097
Pooja Rani satapathy <99600904+Pooja2march@users.noreply.github.com>
2022-02-13 19:49:31 +0530

    Add files via upload

---
+++ week 1 q&a 1.c
@@ -0,0 +1,12 @@
+// c program to display your basic information
+
+#include <stdio.h>
+
+int main()
+{
+  printf("My name is pooja rani satapathy\n");
+  printf("My Regd no-014\n");
+  printf("Branch-Metallurgical and Materials Engineering\n");
+  printf("My hobbies are-singing, sports\n");
+  return 0;
+}
